82b9f0d98a5ab6ae1478b63aec38e19416a665c0
jviitsm <jvictorgmodesto@gmail.com>
2017-04-11 17:16:01 -0300

    alo

--- core/src/br/edu/leaosampaio/GameIC.java
+++ core/src/br/edu/leaosampaio/GameIC.java
@@ -8,70 +8,82 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameIC extends ApplicationAdapter {
 
     private SpriteBatch batch;
-    private Texture fundo;
-	private Texture personagem;
+    private Texture fundo,fundo2;
+	private Texture[] personagem;
+
 
 
     //Atributos de configuração
     private float alturaDispositivo;
     private float larguraDispositivo;
-    private float posicaoInicialVertical;
     private float deltaTime;
+    private float posicaoMovimentoHorizontal;
+    private float getPosicaoMovimentoHorizontal2;
+    private float variacao =0;
 
-    //Camera
-    private OrthographicCamera camera;
-    private Viewport viewport;
-    private final float VIRUAL_WIDTH =768;
-    private final float VIRUAL_HEIGHT =1366;
+
+
 
 	@Override
 	public void create () {
         batch = new SpriteBatch();
-        fundo = new Texture("fundo1.png");
 
-        personagem = new Texture("passaro1.png");
+        fundo = new Texture("city_night.jpg");
+        fundo2 = new Texture("city_night.jpg");
+        personagem = new Texture[3];
+        personagem[0] = new Texture("man_stand.png");
+        personagem[1]= new Texture("man_walk1.png");
+        personagem[2] = new Texture("man_walk2.png");
 
-        alturaDispositivo = Gdx.graphics.getHeight();
-        larguraDispositivo = Gdx.graphics.getWidth();
+      alturaDispositivo = Gdx.graphics.getHeight();
+      larguraDispositivo = Gdx.graphics.getWidth();
 
-        posicaoInicialVertical = alturaDispositivo /2;
 
-        //Cãmera
-        camera = new OrthographicCamera();
-        camera.position.set(VIRUAL_WIDTH /2,VIRUAL_HEIGHT /2,0);
-        viewport = new StretchViewport(VIRUAL_WIDTH,VIRUAL_HEIGHT,camera);
+
 
 	}
 
 	@Override
 	public void render () {
-        camera.update();
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
         deltaTime = Gdx.graphics.getDeltaTime();
 
-        batch.setProjectionMatrix(camera.combined);
+        posicaoMovimentoHorizontal -= deltaTime * 200;
+
+        variacao += deltaTime * 5;
+        if(variacao >2) variacao =0;
+
+        if(posicaoMovimentoHorizontal < fundo.g)
+
 
         batch.begin();
 
+        batch.draw(fundo,posicaoMovimentoHorizontal ,0 , larguraDispositivo,alturaDispositivo);
+        batch.draw(fundo2,posicaoMovimentoHorizontal + larguraDispositivo ,0,larguraDispositivo,alturaDispositivo);
+        batch.draw(personagem[(int) variacao],1,120);
 
-        batch.draw(fundo,0 ,0 , larguraDispositivo,alturaDispositivo);
-        batch.draw(personagem,120,posicaoInicialVertical);
 
 
         batch.end();
+
+
+
+
+
+
+
+
 	}
 
-    @Override
-    public void resize(int width, int height) {
-        viewport.update(width,height);
-    }
 
     @Override
 	public void dispose () {
